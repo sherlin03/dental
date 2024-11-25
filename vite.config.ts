3b7d8cb1745a92786526859f787d8ed7b016508c
sherlin03 <michaelsherlin03@gmail.com>
2024-11-25 14:15:01 +0530

    update

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/dental/',
   plugins: [react()],
+  build: {
+    outDir: 'build', // This sets the output directory to 'build'
+  },
 })
